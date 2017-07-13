069212a04affae8901759840692f113078b4a1f6
unrealwork <unrealwork@gmail.com>
2017-07-13 16:16:09 +0300

    fix edit component

--- client/src/app/edit-node/edit-node.component.ts
+++ client/src/app/edit-node/edit-node.component.ts
@@ -1,24 +1,37 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import {Description} from '../models/description.model';
-import {ActionPopupComponent} from '../action-popup/action-popup.component';
-import {Node} from '../models/node.model';
 import {NodeComponent} from '../node/node.component';
 import {NodePathComponent} from '../node-path/node-path.component';
 import {Message} from '../models/message.model';
 import {MessageType} from '../models/message-type.model';
+import {ActionPopupComponent} from '../action-popup/action-popup.component';
+import {ApiService} from '../services/api.service';
 
 @Component({
   selector: 'app-edit-node',
   templateUrl: './edit-node.component.html',
   styleUrls: ['./edit-node.component.css']
 })
-export class EditNodeComponent extends ActionPopupComponent implements OnInit, AfterViewInit {
-  @Output() addedNode = new EventEmitter<Node>();
-  @Output() featureChanged = new EventEmitter<string>();
-  description: Description = new Description('');
+export class EditNodeComponent implements OnInit, AfterViewInit {
+  @Input() @Output() featureChanged = new EventEmitter<string>();
   @Input() nodeComponent: NodeComponent;
   @Input() pathComponent: NodePathComponent;
   @Output() messageChanged = new EventEmitter<Message>();
+  @Input() header;
+  @ViewChild(ActionPopupComponent) popup;
+  description: Description = new Description('');
+
+  constructor(private api: ApiService) {
+
+  }
 
   ngOnInit(): void {
   }
@@ -27,18 +40,18 @@ export class EditNodeComponent extends ActionPopupComponent implements OnInit, A
     this.description = new Description('');
   }
 
-  addChild(content: string) {
+  update(content: string) {
     const desc = new Description(content);
     this.api.update(this.nodeComponent.node.id, desc).then(node => {
       this.nodeComponent.node.content = node.content;
-      this.pathComponent.refresh();
-      this.close();
       this.description = new Description('');
       const message = new Message('Node name successfuly changed!',
         `Node has new name ${node.content.name}`,
         MessageType.POSITIVE);
       message.timeout = 3000;
       this.messageChanged.emit(message);
+      this.pathComponent.refresh();
+      this.popup.close();
     }).catch(err => {
       this.messageChanged.emit(Message.fromError(err));
     });
